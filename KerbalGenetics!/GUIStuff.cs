cb207632bbb45b21950a8643487ef030dce47e28
wased89 <SilverFox8124@yahoo.ca>
2014-12-07 16:23:17 -0500

    Added Files!
    Stuff!

---
+++ KerbalGenetics!/GUIStuff.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using KSP.IO;
+
+namespace KerbalGenetics
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    class GUIStuff : MonoBehaviour
+    {
+        //Public Variables
+
+        //Private Variables
+        private static Rect mainGUI = new Rect(100,100,100,100);
+
+        void OnStart()
+        {
+
+        }
+
+        void OnAwake()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+
+        }
+
+        void OnGui()
+        {
+
+        }
+
+        void OnWindow()
+        {
+
+        }
+
+
+    }
+}
---
+++ KerbalGenetics!/KerbalGenes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KSP.IO;
+using UnityEngine;
+
+namespace KerbalGenetics
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    public class KerbalGenes : MonoBehaviour
+    {
+    }
+}
